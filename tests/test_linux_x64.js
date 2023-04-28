1817911d48ef72f7bfb48e3cb00b4739fed8dd65
Sara Mentr <saramentr@gmail.com>
2023-04-28 09:28:04 +0300

    Update test_linux_x64.js

--- tests/test_linux_x64.js
+++ tests/test_linux_x64.js
@@ -1,7 +1,7 @@
 import * as config from "../fuzz/config.js";
 
 // if you want to modify config vars you need to do it before including the fuzz module
-config.MAP_SIZE = 128;
+config.set_MAP_SIZE(128);
 
 import * as fuzz from "../fuzz/index.js";
 
@@ -13,14 +13,14 @@ var ARGS_TYPES = ['pointer', 'int'];
 // { traps: 'all' } is needed for stalking
 var func_handle = new NativeFunction(TARGET_FUNCTION, RET_TYPE, ARGS_TYPES, { traps: 'all' });
 
-fuzz.target_module = TARGET_MODULE;
+fuzz.set_target_module(TARGET_MODULE);
 
-fuzz.fuzzer_test_one_input = function (/* Uint8Array */ payload) {
+arg = function (/* Uint8Array */ payload) {
 
   var payload_mem = payload.buffer.unwrap();
 
   func_handle(payload_mem, payload.length);
 
 }
-
+fuzz.set_fuzzer_test_one_input(arg);
 console.log (" >> Agent loaded!");
