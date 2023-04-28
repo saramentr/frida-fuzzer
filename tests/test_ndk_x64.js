f09606797b1b6440e5aa35c37d81bf7b4734ee6b
Sara Mentr <saramentr@gmail.com>
2023-04-28 11:20:46 +0300

    Update test_ndk_x64.js

--- tests/test_ndk_x64.js
+++ tests/test_ndk_x64.js
@@ -1,9 +1,9 @@
-var config = require("../fuzz/config.js");
+import * as config from "../fuzz/config.js";
 
 // if you want to modify config vars you need to do it before including the fuzz module
-config.MAP_SIZE = 128;
+config.set_MAP_SIZE(128);
 
-var fuzz = require("../fuzz");
+import * as fuzz from "../fuzz/index.js";
 
 var TARGET_MODULE = "libnative-lib.so";
 var TARGET_FUNCTION = Module.findExportByName(TARGET_MODULE, "target_func");
@@ -13,14 +13,14 @@ var ARGS_TYPES = ['pointer', 'int'];
 // { traps: 'all' } is needed for stalking
 var func_handle = new NativeFunction(TARGET_FUNCTION, RET_TYPE, ARGS_TYPES, { traps: 'all' });
 
-fuzz.target_module = TARGET_MODULE;
+fuzz.set_target_module(TARGET_MODULE);
 
-fuzz.fuzzer_test_one_input = function (/* Uint8Array */ payload) {
+let arg = function (/* Uint8Array */ payload) {
 
   var payload_mem = payload.buffer.unwrap();
 
   func_handle(payload_mem, payload.length);
 
 }
-
+fuzz.set_fuzzer_test_one_input(arg);
 console.log (" >> Agent loaded!");
